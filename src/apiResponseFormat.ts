972ccb47a3f0a57ce2ce988d0670027e3497cc3d
Christophe FEIJOO <cfeijoo@e2r13p12.42.fr>
2016-09-28 15:05:55 +0200

    Update IApiUser

--- src/apiResponseFormat.ts
+++ src/apiResponseFormat.ts
@@ -30,115 +30,90 @@ export interface IApiUser {
   pool_year: string,
   location: string,
   wallet: number,
-  groups: [any],
-  cursus_users: [
-    {
+  groups: any[],
+  cursus_users: {
+    id: number,
+    begin_at: string,
+    end_at: string,
+    grade: string,
+    level: number,
+    skills: any[],
+    cursus_id: number,
+    user: {
       id: number,
-      begin_at: string,
-      end_at: string,
-      grade: string,
-      level: number,
-      skills: [any],
-      cursus_id: number,
-      user: {
-        id: number,
-        login: string,
-        url: string
-      },
-      cursus: {
-        id: number,
-        created_at: string,
-        name: string,
-        slug: string
-      }
-    }
-  ],
-  projects_users: [
-    {
-      id: number,
-      occurrence: number,
-      final_mark: number,
-      status: string,
-      validated?: boolean,
-      current_team_id: number,
-      project: {
-        id: number,
-        name: string,
-        slug: string
-      }
-    }
-  ],
-  achievements: [
-    {
-      id: number,
-      name: string,
-      description: string,
-      tier: string,
-      kind: string,
-      visible: boolean,
-      image: string,
-      nbr_of_success: number,
-      users_url: string
+      login: string,
+      url: string
     },
-    {
+    cursus: {
+      id: number,
+      created_at: string,
+      name: string,
+      slug: string
+    }
+  }[],
+  projects_users: {
+    id: number,
+    occurrence: number,
+    final_mark: number,
+    status: string,
+    'validated?': boolean,
+    current_team_id: number,
+    project: {
       id: number,
       name: string,
-      description: string,
-      tier: string,
-      kind: string,
-      visible: boolean,
-      image: string,
-      nbr_of_success: number,
-      users_url: string
+      slug: string
     }
-  ],
-  titles: [any],
-  partnerships: [
-    {
+  }[],
+  achievements: {
+    id: number,
+    name: string,
+    description: string,
+    tier: string,
+    kind: string,
+    visible: boolean,
+    image: string,
+    nbr_of_success: number,
+    users_url: string
+  }[],
+  titles: any[],
+  partnerships: {
+    id: number,
+    name: string,
+    slug: string,
+    tier: number,
+    url: string,
+    partnerships_users_url: string
+  }[],
+  patroned: {
+    id: number,
+    user_id: number,
+    godfather_id: number,
+    ongoing: boolean,
+    created_at: string,
+    updated_at: string
+  }[],
+  patroning: any[],
+  expertises_users: {
+    id: number,
+    expertise_id: number,
+    interested: boolean,
+    value: number,
+    contact_me: boolean,
+    created_at: string,
+    user_id: number
+  }[],
+  campus: {
+    id: number,
+    name: string,
+    time_zone: string,
+    language: {
       id: number,
       name: string,
-      slug: string,
-      tier: number,
-      url: string,
-      partnerships_users_url: string
-    }
-  ],
-  patroned: [
-    {
-      id: number,
-      user_id: number,
-      godfather_id: number,
-      ongoing: boolean,
+      identifier: string,
       created_at: string,
       updated_at: string
-    }
-  ],
-  patroning: [any],
-  expertises_users: [
-    {
-      id: number,
-      expertise_id: number,
-      interested: boolean,
-      value: number,
-      contact_me: boolean,
-      created_at: string,
-      user_id: number
-    }
-  ],
-  campus: [
-    {
-      id: number,
-      name: string,
-      time_zone: string,
-      language: {
-        id: number,
-        name: string,
-        identifier: string,
-        created_at: string,
-        updated_at: string
-      },
-      users_count: number,
-      vogsphere_id: number
-    }
-  ]
+    },
+    users_count: number,
+    vogsphere_id: number
+  }[]
 }
