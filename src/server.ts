b659cd3599aa72819e2946452f45b5f447c6df66
Christophe Feijoo <christophe@feijoo.fr>
2016-09-29 20:36:13 +0200

    Clean server

--- src/server.ts
+++ src/server.ts
@@ -10,22 +10,14 @@
 
 import * as express from 'express'
 import * as graphqlHTTP from 'express-graphql'
-import { buildASTSchema, parse } from 'graphql'
-
-import config from './config'
-import api from './abstraction'
 
 import schema from './schema'
 
-const { getUsers } = api(config)
-
-const app = express()
-
-app.use('/graphql', graphqlHTTP({
+express()
+.use('/graphql', graphqlHTTP({
   schema,
   graphiql: true
 }))
-
-app.listen(4000, () =>
+.listen(4000, () =>
   console.log('GraphQL 42 Api listening at localhost:4000')
 )
